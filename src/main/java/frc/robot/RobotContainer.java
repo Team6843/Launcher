5531e9eda361ec092ccda9c46997bb2b4455e81d
John Samson <john.samson.2021@smacatholic.org>
2021-01-28 20:37:51 -0600

    Inital Commit

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,73 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.commands.SetSpeedLaunch;
+import frc.robot.commands.StopMotors;
+import frc.robot.commands.VariableSpeedLaunch;
+import frc.robot.subsystems.LauncherSubsystem;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+
+/**
+ * This class is where the bulk of the robot should be declared.  Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls).  Instead, the structure of the robot
+ * (including subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  private final LauncherSubsystem m_launcherSubsystem = new LauncherSubsystem();
+
+  private final SetSpeedLaunch m_setSpeedLaunch = new SetSpeedLaunch(m_launcherSubsystem);
+
+  private final StopMotors m_stopMotors = new StopMotors(m_launcherSubsystem);
+
+  private final VariableSpeedLaunch m_variableSpeedLaunch = new VariableSpeedLaunch(m_launcherSubsystem, ()-> getSliderAxis());
+
+  private final Joystick joystick = new Joystick(0);
+
+
+  /**
+   * The container for the robot.  Contains subsystems, OI devices, and commands.
+   */
+  public RobotContainer() {
+    // Configure the button bindings
+    configureButtonBindings();
+  }
+
+  /**
+   * Use this method to define your button->command mappings.  Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a
+   * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {
+
+    new JoystickButton(joystick, 2).whileHeld(new SetSpeedLaunch(m_launcherSubsystem));
+    new JoystickButton(joystick, 1).whileHeld(new VariableSpeedLaunch(m_launcherSubsystem, ()-> getSliderAxis()));
+  }
+
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An ExampleCommand will run in autonomous
+    return m_stopMotors;
+  }
+
+  public double getSliderAxis(){
+    return joystick.getThrottle();
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/LauncherSubsystem.java
@@ -0,0 +1,53 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+public class LauncherSubsystem extends SubsystemBase {
+  /**
+   * Creates a new DriveSubsystem.
+   */
+
+  private final WPI_TalonSRX topMotor = new WPI_TalonSRX(Constants.topMotor);
+  private final WPI_TalonSRX bottomMotor = new WPI_TalonSRX(Constants.bottomMotor);
+
+
+  public LauncherSubsystem() {
+
+
+    topMotor.set(ControlMode.PercentOutput, 0.0);
+    bottomMotor.set(ControlMode.PercentOutput, 0.0);
+    // Sets the ramp to 0 and control mode to brake, so the motors stop when we say
+    topMotor.configOpenloopRamp(0);
+    bottomMotor.configOpenloopRamp(0);
+    topMotor.setNeutralMode(NeutralMode.Coast);
+    bottomMotor.setNeutralMode(NeutralMode.Coast);
+  }
+
+  @Override
+  public void periodic() { // This method will be called once per scheduler run
+    
+  }
+
+  public void setSpeed() {
+    topMotor.set(ControlMode.PercentOutput, -0.90);
+    bottomMotor.set(ControlMode.PercentOutput, -0.90);
+  }
+
+  public void variableSpeed(double speed) {
+    double posSpeed = Math.sqrt(Math.pow(speed, 2));
+    topMotor.set(ControlMode.PercentOutput, -posSpeed);
+    bottomMotor.set(ControlMode.PercentOutput, -posSpeed);
+  }
+
+  public void stopMotors(){
+    topMotor.set(ControlMode.PercentOutput, 0.0);
+    bottomMotor.set(ControlMode.PercentOutput, 0.0);
+  }
+
+
+}
